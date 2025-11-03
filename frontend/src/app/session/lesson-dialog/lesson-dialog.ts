821185930684753cfee12ffb71dd7bf78fcf5fe2
Juan Delgado <soy.juan.delgado@gmail.com>
2025-11-03 18:40:04 +0100

    refactor: moved images to assets/images

--- frontend/src/app/session/lesson-dialog/lesson-dialog.ts
+++ frontend/src/app/session/lesson-dialog/lesson-dialog.ts
@@ -1,4 +1,4 @@
-import { Component, input, output } from '@angular/core';
+import { Component, computed, input, output, Signal } from '@angular/core';
 import { ConfirmationService } from 'primeng/api';
 import { ButtonModule } from 'primeng/button';
 import { DialogModule } from 'primeng/dialog';
@@ -16,7 +16,17 @@ export class LessonDialog {
   visibleChange = output<boolean>();
   imageSrc = input('');
 
-close() {
+  // 2. Usamos computed() para derivar la ruta completa
+  // Esta señal se actualizará automáticamente si imageSrc cambia.
+  fullImagePath: Signal<string> = computed(() => {
+    const fileName = this.imageSrc();
+    if (!fileName) {
+      return 'assets/images/no-image.jpg'; // O una ruta a una imagen placeholder
+    }
+    return `assets/images/${fileName}`;
+  });
+
+  close() {
     this.visibleChange.emit(false);
   }
 }
